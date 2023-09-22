11c62d1482756fe33811680156d05736106a1614
WangshuXC <1294673511@qq.com>
2023-09-22 14:44:42 +0800

    update Magisk module v1.2

--- Cn_BA_Unlocker/uninstall.sh
+++ Cn_BA_Unlocker/uninstall.sh
@@ -8,11 +8,13 @@ if [ ! -d "/data/media/0/Android/data/com.RoamingStar.BlueArchive" ] && [ ! -d "
 else
     ui_print "替换回原有文件"
     if [ -d "/sdcard/Documents/BA-backups/" ]; then
+        su
         mv -f "/sdcard/Documents/BA-backups/"* "/data/media/0/Android/data/com.RoamingStar.BlueArchive/files/AssetBundls/"
         rm -rf "/sdcard/Documents/BA-backups/"
     fi
 
     if [ -d "/sdcard/Documents/BA-b-backups/" ]; then
+        su
         mv -f "/sdcard/Documents/BA-b-backups/"* "/data/media/0/Android/data/com.RoamingStar.BlueArchive.bilibili/files/AssetBundls/"
         rm -rf "/sdcard/Documents/BA-b-backups/"
     fi
